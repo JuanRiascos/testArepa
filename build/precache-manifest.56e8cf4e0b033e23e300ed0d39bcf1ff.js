0538ed7d50c7645db4671a56ec2c2898693b0337
Juan David Restrepo <jua.restrepo@udla.edu.co>
2020-07-06 05:19:39 -0500

    release(deploy):Firebase

---
+++ build/precache-manifest.56e8cf4e0b033e23e300ed0d39bcf1ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798ef46255ca3a2d883843bcdf56ebb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "708001fe2eba64ae1c32",
+    "url": "/static/css/2.f0512b8f.chunk.css"
+  },
+  {
+    "revision": "24ee7cb0a88f2c9e6c79",
+    "url": "/static/css/main.47c3fa1f.chunk.css"
+  },
+  {
+    "revision": "708001fe2eba64ae1c32",
+    "url": "/static/js/2.9a07d142.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a07d142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ee7cb0a88f2c9e6c79",
+    "url": "/static/js/main.a27ae019.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  },
+  {
+    "revision": "10aa36d867fa0094b0b3cee6207678ea",
+    "url": "/static/media/Icon.10aa36d8.png"
+  }
+]);
